399dacaa75bb1327367e37e4f65c3cb003c2060c
siddharthxlodhi <siddharthxlodhi@gmail.com>
2025-06-06 21:07:30 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	chitchathub/Dockerfile

--- chit-chat-hub-ui/src/app/pages/main/main.component.ts
+++ chit-chat-hub-ui/src/app/pages/main/main.component.ts
@@ -1,15 +1,15 @@
-import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
-import {ChatResponse} from "../../services/models/chat-response";
-import {ChatService} from "../../services/services/chat.service";
-import {ChatListComponent} from "../../components/chat-list/chat-list.component";
-import {MessageService} from "../../services/services/message.service";
-import {ChatWindowComponent} from "../../components/chat-window/chat-window.component";
-import {KeycloakService} from "../../utils/keycloak/keycloak.service";
-import * as Stomp from 'stompjs';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { ChatResponse } from "../../services/models/chat-response";
+import { ChatService } from "../../services/services/chat.service";
+import { ChatListComponent } from "../../components/chat-list/chat-list.component";
+import { MessageService } from "../../services/services/message.service";
+import { ChatWindowComponent } from "../../components/chat-window/chat-window.component";
+import { KeycloakService } from "../../utils/keycloak/keycloak.service";
+import { Client, IMessage } from '@stomp/stompjs';
 import SockJS from "sockjs-client";
-import {Notification} from "./notification";
-import {MessageResponse} from "../../services/models/message-response";
-import {NgIf, NgOptimizedImage} from "@angular/common";
+import { Notification } from "./notification";
+import { MessageResponse } from "../../services/models/message-response";
+import { NgIf, NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-main',
@@ -21,7 +21,7 @@ import {NgIf, NgOptimizedImage} from "@angular/common";
     NgOptimizedImage
   ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit, OnDestroy {
 
@@ -30,8 +30,7 @@ export class MainComponent implements OnInit, OnDestroy {
 
   chatMessages: MessageResponse[] = [];
 
-  socketClient: any = null;
-
+  private socketClient!: Client;
   private notificationSubscription: any;
 
   constructor(
@@ -39,8 +38,7 @@ export class MainComponent implements OnInit, OnDestroy {
     private messageService: MessageService,
     private keyCloakService: KeycloakService,
     private ngZone: NgZone
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.initWebSocket();
@@ -48,10 +46,11 @@ export class MainComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if (this.socketClient !== null) {
-      this.socketClient.disconnect();
+    if (this.socketClient && this.socketClient.active) {
+      this.socketClient.deactivate();
+    }
+    if (this.notificationSubscription) {
       this.notificationSubscription.unsubscribe();
-      this.socketClient = null;
     }
   }
 
@@ -60,7 +59,7 @@ export class MainComponent implements OnInit, OnDestroy {
       next: (res) => {
         this.chats = res;
       }
-    })
+    });
   }
 
   private getAllChatMessages(chatId: string) {
@@ -70,17 +69,16 @@ export class MainComponent implements OnInit, OnDestroy {
       next: (messages) => {
         this.chatMessages = messages;
       }
-    })
+    });
   }
 
   chatSelectedMethod(chatResponse: ChatResponse) {
     this.selectedChat = chatResponse;
     this.getAllChatMessages(chatResponse.id as string);
     this.setMessagesToSeen();
-    // Update selectedChat's unreadCount immutably
+
     if (this.selectedChat) {
-      this.selectedChat = {...this.selectedChat, unreadCount: 0};
-      // Also update chats array so unreadCount zero is reflected in list
+      this.selectedChat = { ...this.selectedChat, unreadCount: 0 };
       this.chats = this.chats.map(c =>
         c.id === this.selectedChat?.id ? this.selectedChat! : c
       );
@@ -92,34 +90,42 @@ export class MainComponent implements OnInit, OnDestroy {
       'chat-id': this.selectedChat?.id as string
     }).subscribe({
       next: () => {
-        // Optionally update local messages state here if needed
+        // Optional local update
       }
     });
   }
 
   private initWebSocket() {
-    if (this.keyCloakService.keycloak.tokenParsed?.sub) {
-      // let ws = new SockJS('http://localhost:8080/ws');
-      let ws = new SockJS('https://chit-chat-hub-vzan.onrender.com/ws');
-      this.socketClient = Stomp.over(ws);
-      const subUrl = `/user/${this.keyCloakService.keycloak.tokenParsed?.sub}/notification`;
-      this.socketClient.connect({'Authorization': 'Bearer ' + this.keyCloakService.keycloak.token},
-        () => {
-          this.notificationSubscription = this.socketClient.subscribe(subUrl,
-            (message: any) => {
-              const notification: Notification = JSON.parse(message.body);
-              console.log(notification.content);
+    const userId = this.keyCloakService.keycloak.tokenParsed?.sub;
+    const token = this.keyCloakService.keycloak.token;
 
-              // Run inside Angular zone for change detection
-              this.ngZone.run(() => {
-                this.handleNotification(notification);
-              });
-            },
-            () => console.error('Error while connecting to webSocket')
-          );
-        }
-      );
-    }
+    if (!userId || !token) return;
+
+    const wsUrl = 'https://chit-chat-hub-vzan.onrender.com/ws';
+    const subUrl = `/user/${userId}/notification`;
+
+    this.socketClient = new Client({
+      webSocketFactory: () => new SockJS(wsUrl),
+      connectHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+      reconnectDelay: 5000,
+      onConnect: () => {
+        this.notificationSubscription = this.socketClient.subscribe(subUrl, (message: IMessage) => {
+          const notification: Notification = JSON.parse(message.body);
+
+          this.ngZone.run(() => {
+            this.handleNotification(notification);
+          });
+        });
+      },
+      onStompError: frame => {
+        console.error('Broker reported error: ', frame.headers['message']);
+        console.error('Details: ', frame.body);
+      }
+    });
+
+    this.socketClient.activate();
   }
 
   private handleNotification(notification: Notification) {
@@ -128,20 +134,20 @@ export class MainComponent implements OnInit, OnDestroy {
     if (this.selectedChat && this.selectedChat.id === notification.chatId) {
       switch (notification.type) {
         case 'MESSAGE':
-        case 'IMAGE':
+        case 'IMAGE': {
           const message: MessageResponse = {
             senderId: notification.senderId,
             receiverId: notification.receiverId,
             content: notification.content,
             type: notification.messageType,
             media: notification.media,
-            createdAt: new Date().toString()
+            createdAt: new Date().toISOString()
           };
 
           const updatedSelectedChat: ChatResponse = {
             ...this.selectedChat,
             lastMessage: notification.type === 'IMAGE' ? 'Attachment' : notification.content,
-            lastMessageTime: new Date().toString()
+            lastMessageTime: new Date().toISOString()
           };
           this.selectedChat = updatedSelectedChat;
 
@@ -151,9 +157,10 @@ export class MainComponent implements OnInit, OnDestroy {
 
           this.chatMessages = [...this.chatMessages, message];
           break;
+        }
 
         case 'SEEN':
-          this.chatMessages = this.chatMessages.map(m => ({...m, state: 'SEEN'}));
+          this.chatMessages = this.chatMessages.map(m => ({ ...m, state: 'SEEN' }));
           break;
       }
     } else {
@@ -162,7 +169,7 @@ export class MainComponent implements OnInit, OnDestroy {
         const updatedChat: ChatResponse = {
           ...destChat,
           lastMessage: notification.type === 'IMAGE' ? 'Attachment' : notification.content,
-          lastMessageTime: new Date().toString(),
+          lastMessageTime: new Date().toISOString(),
           unreadCount: (destChat.unreadCount || 0) + 1
         };
 
@@ -177,11 +184,10 @@ export class MainComponent implements OnInit, OnDestroy {
           lastMessage: notification.content,
           name: notification.chatName,
           unreadCount: 1,
-          lastMessageTime: new Date().toString()
+          lastMessageTime: new Date().toISOString()
         };
         this.chats = [newChat, ...this.chats];
       }
     }
   }
-
 }
